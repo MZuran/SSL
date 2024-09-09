bf7b0d1d1fc482f44ae2f02d7ee8647b69eacbb6
MZuran <mzuran@frba.utn.edu.ar>
2024-09-09 22:06:42 +0000

    Añadidas funciones para letras y digitos

--- 02-Mini/TP2/scanner.c
+++ 02-Mini/TP2/scanner.c
@@ -20,6 +20,14 @@ bool centinela(t_token estado) {
     }
 }
 
+bool es_letra(char i) {
+    return (i >= 'a' && i <= 'z');
+}
+
+bool es_digito(char c) {
+    return (c >= '0' && c <= '9');
+}
+
 void scan(char *input, size_t len) {
     for (int i = 0; input[i] != '\0'; i++) {
         printf("Caracter en la posición %d: %c\n", i, input[i]);
